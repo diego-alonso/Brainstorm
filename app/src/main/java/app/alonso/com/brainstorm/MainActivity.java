1365717a9039178f9493b714ec4a21f48c5a1272
diego-alonso <diego.alonso.barcena@gmail.com>
2016-04-27 20:28:52 -0500

    Better integration of the myTouchEventView with the parent RelativeLayout

--- app/src/main/java/app/alonso/com/brainstorm/MainActivity.java
+++ app/src/main/java/app/alonso/com/brainstorm/MainActivity.java
@@ -1,31 +1,49 @@
+/*
+ * Copyright (C) 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package app.alonso.com.brainstorm;
 
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import java.io.File;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables for Drawing
+    RelativeLayout relativeLayout;
+    RelativeLayout.LayoutParams params;
+    public Button buttonReset;
+    public Button buttonRandomColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MyTouchEventView touch = new MyTouchEventView(this);
-        setContentView(touch);
-        addContentView(touch.parentLinearLayout, touch.params);
 
-        /*if(isExternalStorageWritable()) {
+        // Buttons
+        buttonReset = (Button) findViewById(R.id.buttonReset);
+        buttonRandomColor = (Button) findViewById(R.id.buttonRandomColor);
 
-        }
-
-        else {
-            Toast.makeText(this, "No storage present", Toast.LENGTH_SHORT).show();
-        }*/
+        //For Drawing
+        relativeLayout = (RelativeLayout) findViewById(R.id.parentRelativeLayout);
+        params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        MyTouchEventView touch = new MyTouchEventView(this, buttonReset, buttonRandomColor);
+        touch.setLayoutParams(params);
+        relativeLayout.addView(touch);
     }
 
     @Override
@@ -34,23 +52,4 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         finish();
     }
-    /*
-    // Checks if external storage is available for read and write
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
-    public File getVideoStorageDir(String folderName) {
-        // Get the directory for the user's public pictures directory.
-        File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_MOVIES), folderName);
-        if (!file.mkdirs()) {
-            Log.e(LOG_TAG, "Directory not created");
-        }
-        return file;
-    }*/
 }
--- app/src/main/java/app/alonso/com/brainstorm/MyTouchEventView.java
+++ app/src/main/java/app/alonso/com/brainstorm/MyTouchEventView.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright (C) 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package app.alonso.com.brainstorm;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,32 +23,19 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.view.MotionEvent;
 import android.view.View;
-//import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.Button;
-import android.widget.LinearLayout;
-//import android.widget.LinearLayout;
+
 import java.util.Random;
 
-/**
- * Created by Diego Alonso on 2/28/2016.
- */
 public class MyTouchEventView extends View {
 
-    private Paint paint = new Paint();
-    private Path path = new Path();
-    private Paint circlePaint = new Paint();
-    private Path circlePath = new Path();
+    public Paint paint = new Paint();
+    public Path path = new Path();
+    public Paint circlePaint = new Paint();
+    public Path circlePath = new Path();
 
-    public Button buttonReset, buttonRandomColor;
-    public ActionBar.LayoutParams params;
-
-    LinearLayout parentLinearLayout;
-
-    public MyTouchEventView(Context context) {
+    public MyTouchEventView(Context context, Button buttonReset, Button buttonRandomColor) {
         super(context);
-
-        params = new ActionBar.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
         paint.setAntiAlias(true);
         paint.setColor(Color.BLACK);
@@ -42,20 +44,12 @@ public class MyTouchEventView extends View {
         paint.setStrokeWidth(5f);
 
         circlePaint.setAntiAlias(true);
-        circlePaint.setColor(Color.BLUE);
+        circlePaint.setColor(Color.BLACK);
         circlePaint.setStyle(Paint.Style.STROKE);
         circlePaint.setStrokeJoin(Paint.Join.MITER);
         circlePaint.setStrokeWidth(2f);
 
-        // Linear Layout
-        parentLinearLayout = new LinearLayout(context);
-
-        // Buttons
-        buttonReset = new Button(context);
-        buttonReset.setText("Clear Screen");
-
-        parentLinearLayout.addView(buttonReset);
-
+        // Reset Button
         buttonReset.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
@@ -67,16 +61,39 @@ public class MyTouchEventView extends View {
                 postInvalidate();
             }
         });
+
+        // Random Color Button
+        buttonRandomColor.setOnClickListener(new View.OnClickListener() {
+
+            Random rand = new Random();
+
+            public void onClick(View v) {
+
+                // Generate a random number
+                int randomNumber = rand.nextInt(6) + 1;
+
+                if(randomNumber == 1) {paint.setColor(Color.BLUE);}
+
+                else if(randomNumber == 2) {paint.setColor(Color.GREEN);}
+
+                else if(randomNumber == 3) {paint.setColor(Color.RED);}
+
+                else if(randomNumber == 4) {paint.setColor(Color.YELLOW);}
+
+                else if(randomNumber == 5) {paint.setColor(Color.GRAY);}
+
+                else if(randomNumber == 6) {paint.setColor(Color.MAGENTA);}
+            }
+        });
     }
 
-    @Override
+    //@Override
     protected void onDraw(Canvas canvas) {
-
         canvas.drawPath(path, paint);
         canvas.drawPath(circlePath, circlePaint);
     }
 
-    @Override
+    //@Override
     public boolean onTouchEvent(MotionEvent event) {
 
         // X and Y coordinates of the Event
